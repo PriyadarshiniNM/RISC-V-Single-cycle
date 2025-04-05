7227414249f046a675c2b35d004bec11faa0544e
PriyadarshiniNM <109581074+PriyadarshiniNM@users.noreply.github.com>
2025-04-05 22:42:51 +0530

    Update Instr_Mem.v

--- Instruction Memory/Instr_Mem.v
+++ Instruction Memory/Instr_Mem.v
@@ -10,19 +10,6 @@ module Instr_Mem(rst, PC, RD);
     begin
         RD <= rst ? 32'd0: Mem[PC[31:2]];
     end
-
-  module Instr_Mem(rst, PC, RD);
-    input [31:0] PC;
-    input rst;
-    output reg [31:0] RD;
-
-    //Memory Creation 
-    reg [31:0] Mem [1023:0];
-
-    always @ (PC) 
-    begin
-        RD <= rst ? 32'd0: Mem[PC[31:2]];
-    end
     
   initial begin
     Mem[0] = 32'hFFC4A303;
@@ -32,4 +19,4 @@ module Instr_Mem(rst, PC, RD);
     //Mem[0] = 32'h0062E233;
     // Mem[1] = 32'h00B62423;
   end
-endmodule 
+endmodule
